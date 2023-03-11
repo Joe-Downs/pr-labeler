9911d76c09a51e26788f1f2b0b7a0c7b42824001
Joe Downs <joe@dwns.dev>
2023-03-11 11:20:06 -0500

    remove fake issue; print out milestone on PR

--- github-labeler.py
+++ github-labeler.py
@@ -70,6 +70,7 @@ def ensureMilestones(pullRequest, repo):
             print(f"Setting milestone to '{milestone.title}'")
             pullRequest.edit(milestone=None)
             print(f"Finished setting milestone. Leaving ensureMilestones()")
+            print(f"PR Milestone: {pullRequest.milestone.title}")
             break
     return None
 
@@ -80,8 +81,7 @@ g = Github(GITHUB_TOKEN)
 repo = g.get_repo(GITHUB_REPOSITORY)
 prNum = int(PR_NUM)
 pr = repo.get_pull(prNum)
-fakeIssue = repo.get_issue(9999)
 issue = repo.get_issue(prNum)
 ensureLabels(pr, repo)
 ensureMilestones(issue, repo)
-ensureMilestones(fakeIssue, repo)
+
